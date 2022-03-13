fffa8f4ab7258a85fbd6a438b2981a3560776070
Aditi <aditime18@gmail.com>
2022-03-13 10:27:55 +0530

    homepage

--- pages/clothing/1.js
+++ pages/clothing/1.js
@@ -1,5 +1,45 @@
 export default function One() {
     return(
-        <></>
+        <>
+        <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round"
+              strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">Behind the Closed Doors</span>
+          </a>
+          <nav
+            className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900">Categories</a>
+            <a className="mr-5 hover:text-gray-900">Products</a>
+          </nav>
+          <button
+            className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Cart
+          </button>
+        </div>
+      </header>
+            <section class="text-gray-600 body-font">
+        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+            <img class="object-cover object-center rounded" alt="hero" src="https://i.ibb.co/Q8s3KjD/images-4.jpg" />
+          </div>
+          <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Handcrafted Just for You!
+              <br class="hidden lg:inline-block" />Baluchari Saree
+            </h1>
+            <p class="mb-8 leading-relaxed"> Rs. 5700/- only.
+              Baluchari saree is known for its pallu design which has mythological scenes woven onto it. Finishing one saree takes more than a week.</p>
+            <div class="flex justify-center">
+              <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy</button>
+              <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Add to Cart</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+        </>
     )
 }

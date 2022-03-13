d7cdd2d08c9f841b594b4ed394faa916e3bb968c
Aditi <aditime18@gmail.com>
2022-03-13 11:07:57 +0530

    homepage.js

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,50 +1,53 @@
 import Head from 'next/head'
-
-
+import styles from '../styles/HomePage.module.css'
+import { useState } from 'react';
 
 export default function HomePage() {
-
+    const [data,setData] = useState(`${styles.container} ${styles.rightPanelActive}`)
+        
     return (
+
 
         <>
         <Head>
             <title>Login</title>
         </Head>
-
+        <div className={`${styles.dataContainer}`}>
             
-        <div className="container right-panel-active">
-            <div className="container-form container-signup">
-                <form action="#" className="form" id="form1">
-                    <h2 className="form-title">Sign Up</h2>
-                    <input type="text" placeholder="User" className="input" />
-                    <input type="email" placeholder="Email" className="input" />
-                    <input type="password" placeholder="Password" className="input" />
-                    <button className="btn">Sign Up</button>
+        <div className={data}>
+            <div className={`${styles.containerForm} ${styles.containerSignup}`}>
+                <form action="#" className={`${styles.form}`} id="form1">
+                    <h2 className={`${styles.formTitle}`}>Sign Up</h2>
+                    <input type="text" placeholder="User" className={`${styles.input}`} />
+                    <input type="email" placeholder="Email" className={`${styles.input}`} />
+                    <input type="password" placeholder="Password" className={`${styles.input}`} />
+                    <button className={`${styles.btn}`}>Sign Up</button>
                 </form>
             </div>
         
-            <div className="container-form container-signin">
-                <form action="#" className="form" id="form2">
-                    <h2 className="form-title">Sign In</h2>
-                    <input type="email" placeholder="Email" className="input" />
-                    <input type="password" placeholder="Password" className="input" />
-                    <a href="#" className="link">Forgot your password?</a>
-                    <button className="btn">Sign In</button>
+            <div className={`${styles.containerForm} ${styles.containerSignin}`}>
+                <form action="#" className={`${styles.form}`} id="form2">
+                    <h2 className={`${styles.formTitle}`}>Sign In</h2>
+                    <input type="email" placeholder="Email" className={`${styles.input}`} />
+                    <input type="password" placeholder="Password" className={`${styles.input}`} />
+                    <a href="#" className={`${styles.link}`}>Forgot your password?</a>
+                    <button className={`${styles.btn}`}>Sign In</button>
                 </form>
             </div>
         
-            <div className="container-overlay">
-                <div className="overlay">
-                    <div className="overlay-panel overlay-left">
-                        <button className="btn" id="signIn">Sign In</button>
+            <div className={`${styles.containerOverlay}`}>
+                <div className={`${styles.overlay}`}>
+                    <div className={`${styles.overlayPanel} ${styles.overlayLeft}`}>
+                        <button className={`${styles.btn}`} id="signIn" onClick={()=>{setData(`${styles.container}`)}}>Sign In</button>
                     </div>
-                    <div className="overlay-panel overlay-right">
-                        <button className="btn" id="signUp">Sign Up</button>
+                    <div className={`${styles.overlayPanel} ${styles.overlayRight}`}>
+                        <button className={`${styles.btn}`} id="signUp" onClick={()=>{setData(`${styles.container} ${styles.rightPanelActive}`)}}>Sign Up</button>
                     </div>
                 </div>
             </div>
         </div>
         <script src="../../../script.js"></script>
+        </div>
     </>
     )
   
